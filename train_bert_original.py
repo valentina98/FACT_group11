68476b1bee9592cc91ef88dc72224f0b2c8ba7b9
OliverSavolainen <oliverssavolainen@gmail.com>
2024-02-01 10:36:01 +0100

    Add accuracy after every epoch

--- train_bert_original.py
+++ train_bert_original.py
@@ -15,7 +15,7 @@ def config():
     parser.add_argument("--seed", default=42, type=int, help="Random seed")
     parser.add_argument("--batch-size", default=16, type=int)
     parser.add_argument("--num-workers", default=2, type=int)
-    parser.add_argument("--epochs", default=3, type=int)
+    parser.add_argument("--epochs", default=10, type=int)
     parser.add_argument("--lr", default=1e-2, type=float)
     return parser.parse_args()
 
@@ -61,22 +61,21 @@ def main(args, train_loader, test_loader, classes):
             optim.step()
             optim.zero_grad()
 
-    # Evaluation
-    model.eval()
-    predictions, true_labels = [], []
-    with torch.no_grad():
-        for texts, labels in tqdm(test_loader):
-            labels = labels.to(device)
-            encodings = tokenizer(texts, padding=True, truncation=True, max_length=512, return_tensors='pt')
-            input_ids = encodings['input_ids'].to(device)
-            attention_mask = encodings['attention_mask'].to(device)
+        model.eval()
+        predictions, true_labels = [], []
+        with torch.no_grad():
+            for texts, labels in tqdm(test_loader):
+                labels = labels.to(device)
+                encodings = tokenizer(texts, padding=True, truncation=True, max_length=512, return_tensors='pt')
+                input_ids = encodings['input_ids'].to(device)
+                attention_mask = encodings['attention_mask'].to(device)
 
-            logits = model(input_ids, attention_mask)
-            predictions.extend(logits.argmax(dim=-1).cpu().numpy())
-            true_labels.extend(labels.cpu().numpy())
+                logits = model(input_ids, attention_mask)
+                predictions.extend(logits.argmax(dim=-1).cpu().numpy())
+                true_labels.extend(labels.cpu().numpy())
 
-    accuracy = accuracy_score(true_labels, predictions)
-    print(f"Accuracy: {accuracy}")
+        accuracy = accuracy_score(true_labels, predictions)
+        print(f"Accuracy: {accuracy}")
 
 if __name__ == "__main__":
     args = config()
