Fixing precision

learn_concepts_multimodal.py
@@ -191,7 +191,7 @@ if __name__ == "__main__":
             all_concepts = list(set(all_concepts).difference(set(biased_classes)))
         # Generate the concept bank
         print("all_concepts shape", np.array(all_concepts).shape)
-        learn_conceptbank(args, all_concepts, "coco_biased")
+        learn_conceptbank(args, all_concepts, args.classes)
 
     else:
         raise ValueError(f"Unknown classes: {args.classes}. Define your dataset here!")

train_pcbm_h.py
@@ -8,14 +8,12 @@ import sys
 import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
 from scipy.special import softmax
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, precision_score
 
 from data import get_dataset
 from concepts import ConceptBank
 from models import PosthocLinearCBM, PosthocHybridCBM, get_model
 from training_tools import load_or_compute_projections, AverageMeter, MetricComputer
-
-
 
 def config():
     parser = argparse.ArgumentParser()
@@ -35,7 +33,7 @@ def config():
 
 @torch.no_grad()
 def eval_model(args, posthoc_layer, loader, num_classes):
-    epoch_summary = {"Accuracy": AverageMeter(), "Precision": AverageMeter()}
+    epoch_summary = {"Accuracy": AverageMeter()}
     tqdm_loader = tqdm(loader)
     computer = MetricComputer(n_classes=num_classes)
     all_preds = []
@@ -50,7 +48,6 @@ def eval_model(args, posthoc_layer, loader, num_classes):
 
         metrics = computer(out, batch_Y) 
         epoch_summary["Accuracy"].update(metrics["accuracy"], batch_X.shape[0]) 
-        epoch_summary["Precision"].update(metrics["precision"], batch_X.shape[0])
 
         summary_text = [f"Avg. {k}: {v.avg:.3f}" for k, v in epoch_summary.items()]
         summary_text = "Eval - " + " ".join(summary_text)
@@ -58,6 +55,9 @@ def eval_model(args, posthoc_layer, loader, num_classes):
     
     all_preds = np.concatenate(all_preds, axis=0)
     all_labels = np.concatenate(all_labels, axis=0)
+    pred_classes = np.argmax(all_preds, axis=1)
+    
+    epoch_summary["Precision"] = precision_score(all_labels, pred_classes, average='weighted', zero_division=0)
     
     if all_labels.max() == 1:
         auc = roc_auc_score(all_labels, softmax(all_preds, axis=1)[:, 1])
@@ -68,11 +68,14 @@ def eval_model(args, posthoc_layer, loader, num_classes):
 
 def train_hybrid(args, train_loader, val_loader, posthoc_layer, optimizer, num_classes):
     cls_criterion = nn.CrossEntropyLoss()
+
+    
+    all_train_preds = []
+    all_train_labels = []
     for epoch in range(1, args.num_epochs+1):
         print(f"Epoch: {epoch}")
         epoch_summary = {"CELoss": AverageMeter(),
-                         "Accuracy": AverageMeter(),
-                         "Precision": AverageMeter()}
+                         "Accuracy": AverageMeter()}
         tqdm_loader = tqdm(train_loader)
         computer = MetricComputer(n_classes=num_classes)
         for batch_X, batch_Y in tqdm(train_loader):
@@ -87,11 +90,18 @@ def train_hybrid(args, train_loader, val_loader, posthoc_layer, optimizer, num_c
             epoch_summary["CELoss"].update(cls_loss.detach().item(), batch_X.shape[0])
             metrics = computer(out, batch_Y) 
             epoch_summary["Accuracy"].update(metrics["accuracy"], batch_X.shape[0])
-            epoch_summary["Precision"].update(metrics["precision"], batch_X.shape[0])
+
+            all_train_preds.append(out.detach().cpu().numpy())
+            all_train_labels.append(batch_Y.detach().cpu().numpy())
 
             summary_text = [f"Avg. {k}: {v.avg:.3f}" for k, v in epoch_summary.items()]
             summary_text = " ".join(summary_text)
             tqdm_loader.set_description(summary_text)
+        
+        all_train_preds = np.concatenate(all_train_preds, axis=0)
+        all_train_labels = np.concatenate(all_train_labels, axis=0)
+        train_pred_classes = np.argmax(all_train_preds, axis=1)
+        train_overall_precision = precision_score(all_train_labels, train_pred_classes, average='weighted', zero_division=0)
 
         # Evaluate the model
         test_metrics = eval_model(args, posthoc_layer, val_loader, num_classes)
@@ -102,10 +112,11 @@ def train_hybrid(args, train_loader, val_loader, posthoc_layer, optimizer, num_c
             "args": args,
             "train_acc": epoch_summary["Accuracy"].avg,
             "test_acc": test_metrics["Accuracy"].avg,
-            "train_precision": epoch_summary["Precision"].avg,
+            "train_precision": train_overall_precision,
             "test_precision": test_metrics["Precision"].avg,
             "train_ce_loss": epoch_summary["CELoss"].avg
         }
+
 
         # Print out the metrics for the current epoch
         print(f"Epoch {epoch} summary:")